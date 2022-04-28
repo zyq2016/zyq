24a1dc896605fe46701e2a8f7a9911539d1267c6
@qq619605516 <619605516@qq.com>
2022-04-28 10:21:53 +0800

    up

---
+++ Desktop/entries/cms-manage/src/App.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: zyq
+ * @Date: 2022-04-28 09:44:47
+ * @LastEditors: zyq
+ * @LastEditTime: 2022-04-28 09:49:54
+ * @FilePath: \cms-manage\src\App.tsx
+ * @Description:
+ *
+ * Copyright (c) 2022 by 用户/公司名, All Rights Reserved.
+ */
+import React from "react";
+import { Button } from "antd";
+import "./App.css";
+const App = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default App;
---
+++ Desktop/entries/cms-manage/src/index.tsx
@@ -0,0 +1,14 @@
+/*
+ * @Author: zyq
+ * @Date: 2022-04-28 09:44:33
+ * @LastEditors: zyq
+ * @LastEditTime: 2022-04-28 09:46:31
+ * @FilePath: \cms-manage\src\index.tsx
+ * @Description:
+ *
+ * Copyright (c) 2022 by 用户/公司名, All Rights Reserved.
+ */
+import ReactDom from "react-dom";
+import App from "./App";
+
+ReactDom.render(<App />, document.getElementById("root"));
